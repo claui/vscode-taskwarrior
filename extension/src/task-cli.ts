3a41c1427b6b0761c1bbf4b6b929781ae110f40f
Claudia Pellegrino <claui@users.noreply.github.com>
2022-05-10 00:11:48 +0200

    Add CodeLens provider for dependencies

--- extension/src/task-cli.ts
+++ extension/src/task-cli.ts
@@ -1,4 +1,5 @@
 export { getTaskwarriorVersion } from "./task-cli/version";
+export { getDescriptionsByIdProvider } from "./task-cli/descriptions";
 
 export interface TaskCli {
   run(argv: string[]): Promise<Buffer | string>;
